694cedfdea94f59eab57f822bfeadd1314dbe551
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2024-12-24 22:29:51 +0300

    Add files via upload

---
+++ constructors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+public:
+    Student(){
+        cout<<"Name: DAVID ELIREHEMA AKYOO"<<endl;
+        cout<<"Reg No: 22100523140058"<<endl;
+        cout<<"Course Name: DIPLOMA IN COMPUTER SCINCE"<<endl;
+        cout<<"Academic Year: SECOND YEAR(2024/2025)"<<endl;
+    }
+    };
+int main(){
+    Student details;
+
+    return 0;
+}
